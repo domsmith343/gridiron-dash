2977974a1a9f00a6a28ea1366bbfa34c994b28de
Your Name <you@example.com>
2025-05-26 23:06:49 -0700

    added a pug

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,19 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  vite: {
+    plugins: [
+      {
+        name: 'pug',
+        transform(code, id) {
+          if (id.endsWith('.pug')) {
+            return {
+              code: `export default ${JSON.stringify(code)}`,
+              map: null
+            };
+          }
+        }
+      }
+    ]
+  }
 });
